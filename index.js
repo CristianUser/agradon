772bcffa939c1f16f9f8974de2c070fff8547413
CristianUser <cristianmejia97@gmail.com>
2020-02-02 22:42:46 -0400

    unnecessary dependences deleted

--- index.js
+++ index.js
@@ -1,50 +1,17 @@
-/* eslint-disable guard-for-in */
 'use strict';
-
-const express = require('express'), // temporally
-  _ = require('lodash'),
-  { getModels, getControllers } = require('./lib/utils'),
-  { createMongooseModels } = require('./lib/models'),
-  { createDefaultCRUD } = require('./lib/crud'),
-  controllers = getControllers() || {},
-  entities = createMongooseModels() || {},
-  modelModules = getModels() || {},
+const { loadEntities, loadServices, setMiddlewares } = require('./lib'),
   pkg = require('./package.json');
 
-function setHeaders(app) {
-  app.set('x-powered-by', false);
-  app.use(`${process.env.REST_PATH || ''}`, function(req, res, next) {
-    res.set('X-Powered-By', `Agradon V${pkg.version}`);
-    next();
-  });
-}
-
-function setMiddlewares(app) {
-  app.use(require('body-parser').json());
-  setHeaders(app);
-}
-
-function loadServices() {
-  require('./lib/services/database');
-}
-
-module.exports.init = app => {
+/**
+ * Initializes Agradon
+ * @param {Express.Application} app
+ * @returns {Express.Application}
+ */
+module.exports.init = function(app) {
   loadServices();
   setMiddlewares(app);
-
-  for (const entity in entities) {
-    const entityModel = entities[entity],
-      entityMiddleware = _.get(modelModules, `${entity}.middleware`, []),
-      entityRouter = express.Router();
-
-    if (controllers[entity]) {
-      controllers[entity](entityRouter, entityModel, entityMiddleware);
-    }
-    createDefaultCRUD(entityRouter, entityModel, entityMiddleware);
-
-    app.use(`${process.env.REST_PATH || ''}/${entity}`, entityRouter);
-  }
-  console.log(`Agradon ${pkg.version} Loaded`);
+  loadEntities(app);
+  console.log(`Agradon ${pkg.version} Loaded 👀 ⭐️`);
 
   return app;
 };
